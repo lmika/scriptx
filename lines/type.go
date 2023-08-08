9a1969ad9f58abe553af6264cdd86469d8e5b441
Leon Mika <lmika@lmika.org>
2023-08-08 00:07:44 +0000

    Added a trim space modifier.

--- lines/type.go
+++ lines/type.go
@@ -1,17 +1,27 @@
 package lines
 
-import "github.com/lmika/scriptx"
+import (
+	"strings"
+)
 
-type lineMatcher struct {
+type Matcher struct {
 	matchFn func(n int, line string) bool
 }
 
-func (lm lineMatcher) Match(n int, line string) bool {
+func (lm Matcher) TrimSpace() Matcher {
+	return Matcher{
+		matchFn: func(n int, line string) bool {
+			return lm.matchFn(n, strings.TrimSpace(line))
+		},
+	}
+}
+
+func (lm Matcher) Match(n int, line string) bool {
 	return lm.matchFn(n, line)
 }
 
-func Equals(matchLine string) scriptx.LineMatcher {
-	return lineMatcher{
+func Equals(matchLine string) Matcher {
+	return Matcher{
 		matchFn: func(n int, line string) bool {
 			return line == matchLine
 		},
